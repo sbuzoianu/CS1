2408a54be04da71d7061fcc9a9a714c5d141c0b5
sbuzoianu <stefan@buzoianu.ro>
2017-11-01 15:53:07 +0200

    Initial Commit

---
+++ nrprime/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  nrprime
+//
+//  Created by Buzoianu Stefan on 28/10/17.
+//  Copyright © 2017 Buzoianu Stefan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var initialView: UIView!
+    func checkPrimeWith(value number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        
+        if number <= 3 {
+            return true
+        }
+        
+        //for (i = 2; i <= n/2; i++)
+        for i in 2...(number/2) {
+            if number % i == 0 {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func onTap(_ sender: UIButton) {
+        if let n = textField.text {
+            if let number: Int = Int(n) {
+                if checkPrimeWith(value: number){
+                    statusLabel.text = "Prim"
+                    view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+                }
+                else {
+                    statusLabel.text = "Neprim"
+                    view.backgroundColor = UIColor.green
+                }
+                
+            }
+            else {
+                statusLabel.text = "Nu este numar"
+            }
+        }
+    }
+}
+
